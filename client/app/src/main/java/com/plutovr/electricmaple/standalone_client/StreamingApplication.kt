9bd1398fc5b1bd2c4843ddeaca2fd3f19b2f8b6a
Lubosz Sarnecki <lubosz.sarnecki@collabora.com>
2024-04-02 12:37:29 +0200

    client/StreamingApplication: Set GST_DEBUG_DUMP_DOT_DIR path in Kotlin environment.

--- client/app/src/main/java/com/plutovr/electricmaple/standalone_client/StreamingApplication.kt
+++ client/app/src/main/java/com/plutovr/electricmaple/standalone_client/StreamingApplication.kt
@@ -5,12 +5,19 @@
 package com.plutovr.electricmaple.standalone_client
 
 import android.app.Application;
+import android.system.Os
 import android.util.Log
 import org.freedesktop.gstreamer.GStreamer
 
 class StreamingApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        // TODO: Implement toggling this via `adb shell setprop` as it enables debug plotting
+        val externalFilesDir = getExternalFilesDir(null)
+        Log.d("ElectricMaple", "Will dump GStreamer plots to $externalFilesDir")
+        Os.setenv("GST_DEBUG_DUMP_DOT_DIR", externalFilesDir.toString(), true)
+
         Log.i("ElectricMaple", "StreamingApplication: In onCreate")
         System.loadLibrary("gstreamer_android")
         Log.i("ElectricMaple", "StreamingApplication: loaded gstreamer_android")
