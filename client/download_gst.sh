d19a192da0c63c0ae411f52cdd1c4425a0c3839f
Lubosz Sarnecki <lubosz.sarnecki@collabora.com>
2024-06-05 10:39:56 +0200

    client/download_gst: Bump to 1.24.4.
    * Delete old directory.
    * Use variable for all gst versions.

--- client/download_gst.sh
+++ client/download_gst.sh
@@ -7,13 +7,18 @@
 # Go to where the script is
 cd $(dirname $0)
 
+export GSTREAMER_VERSION=1.24.4
+
+# Delete old gstreamer directory if it exists
+rm -fR ./deps/gstreamer_android
+
 # Create the destination directory if it doesn't exist
 mkdir -p ./deps/gstreamer_android
 
 # Download the file using curl
-curl -L -o ./deps/gstreamer_android/gstreamer-1.0-android-universal-1.22.6.tar.xz https://gstreamer.freedesktop.org/data/pkg/android/1.22.6/gstreamer-1.0-android-universal-1.22.6.tar.xz
+curl -L -o ./deps/gstreamer_android/gstreamer-1.0-android-universal-${GSTREAMER_VERSION}.tar.xz https://gstreamer.freedesktop.org/data/pkg/android/${GSTREAMER_VERSION}/gstreamer-1.0-android-universal-${GSTREAMER_VERSION}.tar.xz
 
 # Extract the contents of the downloaded file to the destination directory
-tar -xf ./deps/gstreamer_android/gstreamer-1.0-android-universal-1.22.6.tar.xz -C ./deps/gstreamer_android
+tar -xf ./deps/gstreamer_android/gstreamer-1.0-android-universal-${GSTREAMER_VERSION}.tar.xz -C ./deps/gstreamer_android
 
-rm ./deps/gstreamer_android/gstreamer-1.0-android-universal-1.22.6.tar.xz
+rm ./deps/gstreamer_android/gstreamer-1.0-android-universal-${GSTREAMER_VERSION}.tar.xz
