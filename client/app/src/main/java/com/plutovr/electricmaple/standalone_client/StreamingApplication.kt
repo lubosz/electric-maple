3e023039b3dcf2edb90549dcedb96245bd66ef36
Lubosz Sarnecki <lubosz.sarnecki@collabora.com>
2024-06-07 13:27:15 +0200

    client: Read system property to toggle Gst plotting.
    To enable plotting call:
    adb shell setprop debug.electric_maple.enable_plotting 1

--- client/app/src/main/java/com/plutovr/electricmaple/standalone_client/StreamingApplication.kt
+++ client/app/src/main/java/com/plutovr/electricmaple/standalone_client/StreamingApplication.kt
@@ -4,19 +4,45 @@
 
 package com.plutovr.electricmaple.standalone_client
 
-import android.app.Application;
+import android.app.Application
 import android.system.Os
 import android.util.Log
 import org.freedesktop.gstreamer.GStreamer
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+
 
 class StreamingApplication : Application() {
+
+    private val ENABLE_PLOTTING_PROPERY_NAME: String = "debug.electric_maple.enable_plotting"
+
+    private fun isPlottingEnabled(): Boolean {
+        var enablePlotting = ""
+
+        val command = arrayOf("/system/bin/getprop", ENABLE_PLOTTING_PROPERY_NAME)
+
+        try {
+            val process = Runtime.getRuntime().exec(command)
+            val reader = BufferedReader(InputStreamReader(process.inputStream))
+            enablePlotting = reader.readLine()
+            reader.close()
+        } catch (e: IOException) {
+            Log.e("ElectricMaple", "Unable to read system property $ENABLE_PLOTTING_PROPERY_NAME.")
+        }
+
+        return enablePlotting.lowercase() in listOf("1", "true").map { it.lowercase() }
+    }
+
     override fun onCreate() {
         super.onCreate()
 
-        // TODO: Implement toggling this via `adb shell setprop` as it enables debug plotting
-        val externalFilesDir = getExternalFilesDir(null)
-        Log.d("ElectricMaple", "Will dump GStreamer plots to $externalFilesDir")
-        Os.setenv("GST_DEBUG_DUMP_DOT_DIR", externalFilesDir.toString(), true)
+        if (isPlottingEnabled()) {
+            Log.i("ElectricMaple", "Enabling GStreamer pipeline plotting")
+            val externalFilesDir = getExternalFilesDir(null)
+            Log.i("ElectricMaple", "Will dump GStreamer plots to $externalFilesDir")
+            Os.setenv("GST_DEBUG_DUMP_DOT_DIR", externalFilesDir.toString(), true)
+        }
 
         Log.i("ElectricMaple", "StreamingApplication: In onCreate")
         System.loadLibrary("gstreamer_android")
